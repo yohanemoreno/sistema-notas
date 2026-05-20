6c2fd0361a9f90aa8cd29087392deb6b03bb6d8f
Yohane Moreno <yohane.moreno@edu.senai.br>
2026-05-20 10:34:37 -0300

    feat: adiciona notas e cálculo de média

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main() {
 //Entrada - Declaração das variáveis
 string nomes[20];
 int qtdAlunos;
+float notas[20][5];
+float media[20];
+int qtdDisciplinas;
 
 
 //Processamento
@@ -22,6 +25,24 @@ cin.ignore();
 for (int i = 0; i < qtdAlunos; i++) {
     cout << "Nome do aluno: " << i + 1 << ": ";
     getline(cin, nomes[i]);
+}
+
+do {
+    cout << "\nQuantidade de disciplinas (1 a 5): " << endl;
+    cin >> qtdDisciplinas;
+} while (qtdDisciplinas < 1 || qtdDisciplinas > 5);
+
+for ( int i = 0; i < qtdAlunos; i++) {
+cout << "\nNotas de " << nomes[i] << ":" << endl;
+float soma = 0;
+for (int j = 0; j < qtdDisciplinas; j++) {
+    do {
+        cout << "Disciplina " << j + 1 << " (1 a 10): ";
+        cin >> notas[i][j];
+    } while (notas[i][j] < 1 || notas[i][j] > 10);
+    soma += notas[i][j];
+}
+media[i] = soma / qtdDisciplinas;
 }
 
 //Saída
